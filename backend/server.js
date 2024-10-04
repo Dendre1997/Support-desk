ba97df3d072e4b1b16b3d22af5f0d7882d17f072
Dendre1997@ <denyskichuk97@gmail.com>
2024-10-03 23:02:28 -0600

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config() //For using evironment variables
@@ -21,6 +22,20 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+
+// Serve Frontend
+// Check if we in production
+if (process.env.NODE_ENV === 'production') {
+    // set build folder as static /static path 
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    // This route handler catches all GET requests and sends the index.html file from the build directory.
+// It is typically used in a single-page application (SPA) to ensure that all routes are handled by the frontend.
+// __dirname refers to the directory of the current module, and the path is constructed to point to the index.html file.
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    res.status(200).json({message: "Welcome to the support desk api"})
+}
 app.use(errorHandler)
 
 app.listen(PORT, () => {
